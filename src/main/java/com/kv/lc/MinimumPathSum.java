34159a4fbe3aba2a57aba18d32edc486dec17e65
karanverma <karan2.verma@paytmmall.com>
2019-02-15 16:48:58 +0530

    more complex algorithms

---
+++ src/main/java/com/kv/lc/MinimumPathSum.java
@@ -0,0 +1,94 @@
+package com.kv.lc;
+
+/**
+ * 
+ * @author karanverma
+ *
+ *  Given a m x n grid filled with non-negative numbers, find a path from top left to bottom 
+ *  right which minimizes the sum of all numbers along its path.
+ *  Note: You can only move either down or right at any point in time.
+ *  
+ *  Input:
+ *  [
+ *      [1,3,1],
+ *      [1,5,1],
+ *      [4,2,1]
+ *  ]
+ *  Output: 7
+ *  Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ */
+public class MinimumPathSum {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,3,1},{1,5,1},{4,2,1}};
+        
+        System.out.println("MinimumPathSum DFS : "+minPathSum(matrix));
+        
+        
+        System.out.println("MinimumPathSum DP : "+minPathSumDP(matrix));
+    }
+    
+    /*
+     * A native solution would be depth-first search. It's time is too expensive and fails the online judgement.
+     */
+    public static int minPathSum(int[][] grid) {
+        return dfs(0,0,grid);
+    }
+     
+    public static int dfs(int i, int j, int[][] grid){
+        if(i==grid.length-1 && j==grid[0].length-1){
+            return grid[i][j];
+        }
+     
+        if(i<grid.length-1 && j<grid[0].length-1){
+            int r1 = grid[i][j] + dfs(i+1, j, grid);
+            int r2 = grid[i][j] + dfs(i, j+1, grid);
+            return Math.min(r1,r2);
+        }
+     
+        if(i<grid.length-1){
+            return grid[i][j] + dfs(i+1, j, grid);
+        }
+     
+        if(j<grid[0].length-1){
+            return grid[i][j] + dfs(i, j+1, grid);
+        }
+     
+        return 0;
+    }
+
+    // Dynamic Programming
+    public static int minPathSumDP(int[][] grid) {
+        if(grid == null || grid.length==0)
+            return 0;
+     
+        int m = grid.length;
+        int n = grid[0].length;
+     
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];    
+     
+        // initialize top row
+        for(int i=1; i<n; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+     
+        // initialize left column
+        for(int j=1; j<m; j++){
+            dp[j][0] = dp[j-1][0] + grid[j][0];
+        }
+     
+        // fill up the dp table
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(dp[i-1][j] > dp[i][j-1]){
+                    dp[i][j] = dp[i][j-1] + grid[i][j];
+                }else{
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
+                }
+            }
+        }
+     
+        return dp[m-1][n-1];
+    }
+}
---
+++ src/main/java/com/kv/lc/UniquePaths.java
@@ -0,0 +1,116 @@
+package com.kv.lc;
+
+/**
+ * 
+ * @author karanverma
+ *
+ *  A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ *  The robot can only move either down or right at any point in time. The robot is trying to 
+ *  reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ *  How many possible unique paths are there?
+ *  
+ *  Input: m = 3, n = 2 
+ *  Output: 3
+ *  Explanation:
+ *  From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ *  1. Right -> Right -> Down
+ *  2. Right -> Down -> Right
+ *  3. Down -> Right -> Right
+ */
+public class UniquePaths {
+
+    public static void main(String[] args) {
+        int m = 3, n = 3;
+        System.out.println("Unique path DFS : "+uniquePaths(m, n));
+        System.out.println("Unique path DP : "+uniquePathsDp(m, n));
+
+        System.out.println("Unique path DP Memo : "+uniquePathsDPMemo(m, n));
+
+    }
+    
+    //A depth-first search solution is pretty straight-forward. However, the time of this solution 
+    //is too expensive, and it didn't pass the online judge.
+    public static int uniquePaths(int m, int n) {
+        return dfs(0, 0, m, n);
+    }
+
+    public static int dfs(int i, int j, int m, int n) {
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+
+        if (i < m - 1 && j < n - 1) {
+            return dfs(i + 1, j, m, n) + dfs(i, j + 1, m, n);
+        }
+
+        if (i < m - 1) {
+            return dfs(i + 1, j, m, n);
+        }
+
+        if (j < n - 1) {
+            return dfs(i, j + 1, m, n);
+        }
+
+        return 0;
+    }
+    
+    // Dynamic Programming
+    public static int uniquePathsDp(int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (m == 1 || n == 1)
+            return 1;
+
+        int[][] dp = new int[m][n];
+
+        // left column
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // top row
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // fill up the dp table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    // Dynamic Programming with Memorization
+    public static int uniquePathsDPMemo(int m, int n) {
+        int[][] mem = new int[m][n];
+
+        // init with -1 value
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                mem[i][j] = -1;
+            }
+        }
+
+        return helper(mem, m - 1, n - 1);
+    }
+
+    private static int helper(int[][] mem, int m, int n) {
+        // edge has only one path
+        if (m == 0 || n == 0) {
+            mem[m][n] = 1;
+            return 1;
+        }
+
+        if (mem[m][n] != -1) {
+            return mem[m][n];
+        }
+
+        mem[m][n] = helper(mem, m, n - 1) + helper(mem, m - 1, n);
+
+        return mem[m][n];
+    }
+
+}
